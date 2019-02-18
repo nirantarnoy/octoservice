d3a5d7631a2dc53490250e237038c5da12e3dc7e
tarlektarnoy <niran_tarlek@hotmail.com>
2019-02-18 11:56:01 +0700

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var request = require('request');
 //var url = "http://localhost/octo/backend/web/index.php?r=site/index";
 var url = "http://backend.octopus.co.th";
 
-setInterval(checknotify, 15000)
+setInterval(checknotify, 7200000) // 2 ชั่วโมง // 1 minute = 60000
 
 function checknotify(){
     request(url ,function (error, response, body) {
