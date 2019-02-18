2765b8c43f34b702f3c8dd18c7dc6b86fe5b284e
tarlektarnoy <niran_tarlek@hotmail.com>
2019-02-18 12:06:19 +0700

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 //var link = "http://www.cameliso.net/alert/api/index.php?";
 //var url = "http://localhost/octo/backend/web/index.php?r=site/index";
-var url = "http://backend.octopus.co.th";
+var url = "http://backend.octopus.co.th/index.php?r=site/calorder";
 
 setInterval(checknotify, 7200000) // 2 ชั่วโมง // 1 minute = 60000
 
