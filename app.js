f6f7b021424d3273b46552389b158e47ec8943b0
tarlektarnoy <niran_tarlek@hotmail.com>
2019-02-19 08:14:06 +0700

    'update'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var request = require('request');
 //var url = "http://localhost/octo/backend/web/index.php?r=site/index";
 var url = "http://backend.octopus.co.th/index.php?r=site/calorder";
 
-setInterval(checknotify, 7200000) // 2 ชั่วโมง // 1 minute = 60000
+setInterval(checknotify, 14400000) // 2 ชั่วโมง // 1 minute = 60000
 
 function checknotify(){
     request(url ,function (error, response, body) {
